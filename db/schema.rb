e7f3798be33d85f654e96f331acb71963088e1da
Nikki Stevens <nikkilstevens@gmail.com>
2016-06-03 09:35:36 -0700

    forreals links

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603025218) do
+ActiveRecord::Schema.define(version: 20160603032658) do
 
   create_table "bootcamps", force: :cascade do |t|
     t.string   "name",                limit: 255
